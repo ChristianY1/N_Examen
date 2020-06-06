807a181fbfe4a14e928ee03c8540fbe4b576464d
ChristianY1 <christianyunga@gmail.com>
2020-06-06 17:20:04 -0500

    Restart

---
+++ src/ec/edu/ups/Entidades/Autor.java
@@ -0,0 +1,87 @@
+package ec.edu.ups.Entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Autor
+ *
+ */
+@Entity
+
+public class Autor implements Serializable {
+
+
+	
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue
+	private int codigo;
+	private String nombre;
+	private String nacionalidad;
+	@OneToOne
+	@JoinColumn
+	private Capitulo capitulo;
+	
+
+	public Autor() {
+		
+	}
+
+
+	public Autor(String nombre, String nacionalidad, Capitulo capitulo) {
+		super();
+		this.nombre = nombre;
+		this.nacionalidad = nacionalidad;
+		this.capitulo = capitulo;
+	}
+
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+
+	public Capitulo getCapitulo() {
+		return capitulo;
+	}
+
+
+	public void setCapitulo(Capitulo capitulo) {
+		this.capitulo = capitulo;
+	}
+
+
+//	@Override
+//	public String toString() {
+//		return "Autor [codigo=" + codigo + ", nombre=" + nombre + ", nacionalidad=" + nacionalidad + ", capitulo="
+//				+ capitulo + "]";
+//	}
+//   
+   
+}
---
+++ src/ec/edu/ups/Entidades/Capitulo.java
@@ -0,0 +1,103 @@
+package ec.edu.ups.Entidades;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Capitulo
+ *
+ */
+@Entity
+
+public class Capitulo implements Serializable {
+
+	
+
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int codigo;
+	private String nombre;
+	private String titulo;
+	@ManyToOne
+	@JoinColumn
+	private Libro libro;
+	@OneToOne(cascade = CascadeType.ALL,mappedBy = "capitulo")
+	private Autor autor;
+	
+
+	public Capitulo() {
+		
+	}
+
+
+	public Capitulo(String nombre, String titulo, Libro libro) {
+		super();
+		this.nombre = nombre;
+		this.titulo = titulo;
+		this.libro = libro;
+		this.autor = autor;
+	}
+
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+
+	public Libro getLibro() {
+		return libro;
+	}
+
+
+	public void setLibro(Libro libro) {
+		this.libro = libro;
+	}
+
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+
+
+//	@Override
+//	public String toString() {
+//		return "Capitulo [codigo=" + codigo + ", nombre=" + nombre + ", titulo=" + titulo + ", libro=" + libro
+//				+ ", autor=" + autor + "]";
+//	}
+
+
+	
+
+   
+}
